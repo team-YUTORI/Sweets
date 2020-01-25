405026fae70109e50a270296940a674f536102a7
tatuya5911 <kkb76904@gmail.com>
2020-01-25 05:16:46 +0000

    admin/item/new.html & controller [create]

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -7,9 +7,16 @@ class Admins::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,5 +26,11 @@ class Admins::ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :genre_id, :without_tax_price, :sale_status)
   end
 end
