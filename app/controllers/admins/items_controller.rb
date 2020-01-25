f1e229ec3625d9d546bd21088846fcdcea891f19
tatuya5911 <kkb76904@gmail.com>
2020-01-25 06:30:57 +0000

    admin/item/show.html & controller [create]

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -4,16 +4,23 @@ class Admins::ItemsController < ApplicationController
   end
 
   def show
+    item = Item.find(params[:id])
+    genre = Genre.find(params[:id])
+    tax = item.without_tax_price * 1.1   # 税抜価格を所得し、税率10%をかけた
   end
 
   def new
     @item = Item.new
+    @items = Item.find(prams[:id])
+    if @items.user != admin_user
+      redirect_to items_path
+    end
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to items_path
+      redirect_to item_path(item.id)
     else
       render :new
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :without_tax_price, presence: true
+  validates :genre_id, presence: true
+  validates :sale_status, presence: true
 end
