4e9e176277b6180552f61392bb46c693c452010e
KUGA16 <gachan_1216@yahoo.co.jp>
2020-01-27 10:40:39 +0000

    Change add successmessage

--- app/controllers/customers/delivery_addresses_controller.rb
+++ app/controllers/customers/delivery_addresses_controller.rb
@@ -1,9 +1,27 @@
 class Customers::DeliveryAddressesController < ApplicationController
 
   def index
+    @delivery = DeliveryAddress.new
+    @deliverys = DeliveryAddress.all
   end
 
   def create
+    @delivery = DeliveryAddress.new(delivery_address_params)
+
+    if @delivery.save
+       redirect_to delivery_addresses_path
+       flash[:notice] = "配送先が登録されました"
+    else
+      @deliverys = DeliveryAddress.all
+      render :index
+    end
+
+  end
+
+  def destroy
+    delivery = DeliveryAddress.find(params[:id])
+    delivery.destroy
+    redirect_to delivery_addresses_path
   end
 
   def edit
@@ -12,7 +30,9 @@ class Customers::DeliveryAddressesController < ApplicationController
   def update
   end
 
-  def destroy
+  private
+  def delivery_address_params
+    params.require(:delivery_address).permit(:postal_code, :address, :name)
   end
 
 end
