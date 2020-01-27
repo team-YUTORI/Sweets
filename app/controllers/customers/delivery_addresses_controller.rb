90ffaef00ebffbf4bf84b6b31b0b0f2d1f2494a8
KUGA16 <gachan_1216@yahoo.co.jp>
2020-01-27 07:32:29 +0000

    Add action index,create,edit,update,destroy

--- app/controllers/customers/delivery_addresses_controller.rb
+++ app/controllers/customers/delivery_addresses_controller.rb
@@ -1,2 +1,18 @@
 class Customers::DeliveryAddressesController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
