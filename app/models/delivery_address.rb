39d2d9ead5cac47c906f4faf8ac14bed6301ffa0
KUGA16 <gachan_1216@yahoo.co.jp>
2020-01-27 10:04:07 +0000

    Add validates address and name

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,4 +1,9 @@
 class DeliveryAddress < ApplicationRecord
   # 郵便番号バリデーション（空白、文字制限）
   validates :postal_code, presence: true, length: { maximum: 7 }
+  # 住所バリデーション （空白）
+  validates :address, presence: true
+  # 宛名バリデーション （空白）
+  validates :name, presence: true
+  
 end
