3cc920b28d1346639887dad47a9e1cf53aa55a41
KUGA16 <gachan_1216@yahoo.co.jp>
2020-01-28 09:04:20 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_01_27_053946) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "on_display", default: true, null: false
+    t.boolean "on_display"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
