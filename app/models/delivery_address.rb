e80eaebe56c5efeb1fc95309ec659a94b6515497
KUGA16 <gachan_1216@yahoo.co.jp>
2020-01-30 09:03:45 +0000

    Change view_code

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,9 +1,16 @@
 class DeliveryAddress < ApplicationRecord
+
+  belongs_to :customer
   # 郵便番号バリデーション（空白、文字制限）
   validates :postal_code, presence: true, length: { maximum: 7 }
   # 住所バリデーション （空白）
   validates :address, presence: true
   # 宛名バリデーション （空白）
   validates :name, presence: true
+
+  # 郵便番号と住所と名前を一緒に記述
+  def view_code
+    self.postal_code + self.address + self.name
+  end
 
 end
