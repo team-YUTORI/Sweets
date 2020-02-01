c3b484791cf0ca44f51061ab30e9375c26eceeca
tatuya5911 <kkb76904@gmail.com>
2020-02-01 06:09:41 +0000

    ok

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
   has_many :order_details
   has_many :cart_items
   belongs_to :genre
-  belongs_to :home
 
   mount_uploader :item_image_id, ItemImageIdUploader
 
@@ -15,5 +14,5 @@ class Item < ApplicationRecord
   validates :sale_status, presence: true
 
   enum sale_status: [:販売中, :売切れ]
-  
+
 end
