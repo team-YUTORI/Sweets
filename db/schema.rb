3c7b8854060c48aaea892e9756fc0c61cf1a834d
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-01 05:21:24 +0000

    Test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_053946) do
+ActiveRecord::Schema.define(version: 2020_01_28_112812) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_01_27_053946) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "on_display", default: true, null: false
+    t.boolean "on_display"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
