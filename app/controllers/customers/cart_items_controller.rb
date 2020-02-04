aa80aa78dd8fb57fab739ed5bf0148ba9dde1d78
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-04 04:54:13 +0000

    Change レイアウト変更＆カートアイテムで同じ商品だったら数量だけupdate

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -22,7 +22,7 @@ class Customers::CartItemsController < ApplicationController
       cart_item = current_customer.cart_items.new(cart_item_params)
       cart_item.save!
     end
-
+    @cart_items = CartItem.where(customer_id: current_customer)
     redirect_to cart_items_path
   end
 
