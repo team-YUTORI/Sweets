71ed8649cdafed8ee5f2b95cd76ab181154a9298
R-t0923 <t.07r.12@gmail.com>
2020-02-04 12:22:37 +0000

    add image

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,6 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :without_tax_price, presence: true
   validates :genre_id, presence: true
-  validates :item_number, presence: true
   enum sale_status: [:販売中, :売切れ]
 
 end
