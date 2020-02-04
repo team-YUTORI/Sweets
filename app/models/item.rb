4d771d36812d9d61a650bedf290593478364be48
R-t0923 <t.07r.12@gmail.com>
2020-02-04 09:52:18 +0000

    add

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,7 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :without_tax_price, presence: true
   validates :genre_id, presence: true
-  validates :sale_status, presence: true
-
+  validates :item_number, presence: true
   enum sale_status: [:販売中, :売切れ]
 
 end
